a6a548152d8dd7f6f3af5ebb85aa033d1f326201
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-09-11 11:23:34 +0530

    completed the project

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+function preload(){
+    clown_nose = loadImage("https://i.postimg.cc/KzFxdJL1/clownnose2.png");
 }
 
 function setup(){
@@ -21,13 +24,16 @@ function gotPoses(results){
     if(results.length > 0){
         console.log(results);
 
-        console.log("nose x: " + results[0].pose.nose.x);
-        console.log("nose y: " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
+        console.log("nose x: " + noseX);
+        console.log("nose y: " + noseY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 30, 30)
 }
 
 function take_snapshot(){
